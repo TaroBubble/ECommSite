de765d3afbc97294be5055756a72288f68d9f0c7
Kevin Chen <kchen0296@gmail.com>
2021-07-19 13:33:42 -0400

    Parsing Form Data

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('hi there');
+const express = require('express');
+
+const app = express();
+
+//watch for get requests, with a path of ./
+app.get('/', (request, response) => {
+    response.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <input name="passwordConfirmation" placeholder="confirm password" />
+            <button>Sign Up</button>
+        </form>
+    </div>
+    `);
+});
+
+const formParser = (request, response, next) => {
+    if (request.method === 'POST') {
+        request.on('data', (data) => {
+            const parse = data.toString('utf8').split('&');
+            const formData = {};
+            for (let pair of parse) {
+                const [key, value] = pair.split('=');
+                formData[key] = value;
+            }
+            request.body = formData;
+            next();
+        });
+    } else {
+        next();
+    }
+}
+
+app.post('/', formParser , (request, response) => {
+    //parse form data
+    //.on similar to addeventlistener, 
+    console.log(request.body);
+    response.send('acc created');
+})
+
+app.listen(3000, () => {
+    console.log('listening');
+});
