04be3471f31ba8c878a8241a649084880e25a07d
Kevin Chen <kchen0296@gmail.com>
2021-07-21 18:20:39 -0400

    Added html layout

--- Views/admin/auth/signup.js
+++ Views/admin/auth/signup.js
@@ -1,13 +1,15 @@
+const layout = require('../layout');
+
 module.exports = ({request}) => {
-    return `
-    <div>
-        your id is ${request.session.userId}
-        <form method="POST">
-            <input name="email" placeholder="email" />
-            <input name="password" placeholder="password" />
-            <input name="passwordConfirmation" placeholder="confirm password" />
-            <button>Sign Up</button>
-        </form>
-    </div>
-    `;
+    return layout({content: `
+        <div>
+            your id is ${request.session.userId}
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name="passwordConfirmation" placeholder="confirm password" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `});
 }
